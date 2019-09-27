e7abe21a34be6778bd0eca40b60764410762ac95
Pauli Gergő <gergopauli2@gmial.com>
2019-09-27 14:06:02 +0200

    version 0.01

---
+++ Snake/Snake.py
@@ -0,0 +1,11 @@
+#import stuff
+import turtle
+
+#Játékfelület létrehozása
+ablak = turtle.Screen()
+ablak.title("Snake (EGER_2019_1_E_Katica)")
+ablak.bgcolor("lightblue")
+ablak.setup(width=800, height=800)
+ablak.tracer(0)
+
+ablak.mainloop()
